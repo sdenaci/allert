afa1bd1e52903ebfbf7b9b6b8a11b2086cd0ea58
Sarah Denaci <sarahdenaci@gmail.com>
2018-01-18 11:10:59 -0500

    add: very basic styling to recipe squares

--- client/components/recipeSquare.js
+++ client/components/recipeSquare.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { DefaultButton } from './index'
 
-const gridDivStyle = {
-  flexBasis: '30%',
-  borderStyle: 'solid',
-  borderWidth: '2px',
-  borderRadius: '5px',
-  margin: '1rem'
-}
-
 class RecipeSquare extends Component {
 
   constructor() {
@@ -29,24 +21,26 @@ class RecipeSquare extends Component {
     const { recipe } = this.props
 
     return (
-      <div style={gridDivStyle}>
-        <div>
+      <div className="recipeSquareDiv">
+        <div className="recipeSquareElem">
           <h1>{recipe.recipeName}</h1>
           <img src={recipe.imageUrlsBySize[90]} />
-          <p>ingredients:</p>
-          <DefaultButton
-            label={this.state.showIngredients ? 'Hide Ingredients' : 'Show Ingredients'}
-            handleClick={this.handleToggle}
-          />
-          {this.state.showIngredients &&
-            <ul>
-              {
-                recipe.ingredients.map(ingredient =>
-                  <li key={ingredient}>{ingredient}</li>
-                )
-              }
-            </ul>
-          }
+          <div className={this.state.showIngredients ? "display-ingredient" : "hide-ingredient"}>
+            <p>ingredients:</p>
+            <DefaultButton
+              label={this.state.showIngredients ? 'Hide' : 'Show'}
+              handleClick={this.handleToggle}
+            />
+            {this.state.showIngredients &&
+              <ul>
+                {
+                  recipe.ingredients.map(ingredient =>
+                    <li key={ingredient}>{ingredient}</li>
+                  )
+                }
+              </ul>
+            }
+          </div>
         </div>
       </div>
     )
